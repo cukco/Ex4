2fbe47ed78fd10009715722151f74317efdecfbd
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-05-30 18:28:24 +0700

    Ex4 SS5

--- Ex4.sql
+++ Ex4.sql
@@ -1,26 +1,33 @@
-insert into products(id, name, category, price, stock, manufacturer) values
-    ('8','Chuột không dây Logitech M170','Phụ kiện',300000,20,'Logitech');
+select customer_name as "Tên khách",
+       order_date as "Ngày đặt",
+       total_amount as "Tổng tiền"
+from customers
+inner join orders on customers.customer_id = orders.customer_id;
 
-update products
-set price=price*1.1 where manufacturer= 'Apple';
+select sum(total_amount) as "Tổng doanh thu",
+       avg(total_amount) as "Trung bình giá trị đơn hàng",
+       max(total_amount) as "Đơn hàng lớn nhất",
+       min(total_amount) as "Đơn hàng nhỏ nhất",
+       count(order_id) as "Số lượng đơn hàng "
+from orders;
 
-delete from products
-where stock=0;
+select city,sum(total_amount) as  total_revenue from customers
+inner join orders on customers.customer_id = orders.customer_id
+group by city
+having sum(total_amount) >10000;
 
-select *from products
-where price between 1000000 and 30000000;
+select order_items.product_name,order_items.quantity,order_items.price,
+       customers.customer_name,orders.order_date from order_items
+inner join orders on order_items.order_id = orders.order_id
+inner join customers on orders.customer_id = customers.customer_id;
 
-delete from products
-where stock is null;
+select customers.customer_name from customers
+inner join orders on customers.customer_id = orders.customer_id
+group by customer_name, orders.customer_id
+having sum(total_amount) = (
+    select sum(total_amount) from orders
+    group by orders.customer_id
+    order by sum(total_amount) desc
+    limit 1
+);
 
-select distinct manufacturer from products;
-
-select *from products
-order by price desc,name asc;
-
-select *from products
-where name ilike '%laptop%';
-
-select *from products
-order by  price desc
-limit 2;
