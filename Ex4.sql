6d20183baeb16b62c159eac44e8f3ef18c26bfcf
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-06-03 09:39:45 +0700

    Ex4 SS7

--- Ex4.sql
+++ Ex4.sql
@@ -1,33 +1,11 @@
-select customer_name as "Tên khách",
-       order_date as "Ngày đặt",
-       total_amount as "Tổng tiền"
-from customers
-inner join orders on customers.customer_id = orders.customer_id;
+create view v_revenue_by_region as
+select region, sum(total_amount) as total_revenue from customer
+inner join orders on customer.customer_id = orders.customer_id
+group by region;
 
-select sum(total_amount) as "Tổng doanh thu",
-       avg(total_amount) as "Trung bình giá trị đơn hàng",
-       max(total_amount) as "Đơn hàng lớn nhất",
-       min(total_amount) as "Đơn hàng nhỏ nhất",
-       count(order_id) as "Số lượng đơn hàng "
-from orders;
+select * from v_revenue_by_region
+order by total_revenue desc limit 3;
 
-select city,sum(total_amount) as  total_revenue from customers
-inner join orders on customers.customer_id = orders.customer_id
-group by city
-having sum(total_amount) >10000;
-
-select order_items.product_name,order_items.quantity,order_items.price,
-       customers.customer_name,orders.order_date from order_items
-inner join orders on order_items.order_id = orders.order_id
-inner join customers on orders.customer_id = customers.customer_id;
-
-select customers.customer_name from customers
-inner join orders on customers.customer_id = orders.customer_id
-group by customer_name, orders.customer_id
-having sum(total_amount) = (
-    select sum(total_amount) from orders
-    group by orders.customer_id
-    order by sum(total_amount) desc
-    limit 1
-);
-
+create view v_revenue_above_avg as
+select * from v_revenue_by_region
+where total_revenue > (select avg(total_revenue) from v_revenue_by_region);
