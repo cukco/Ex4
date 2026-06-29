c3662f41826b9a05c36ed67c4ee6ecc687a0cc8e
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-06-29 11:41:27 +0700

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -1,11 +1,25 @@
 import java.util.Scanner;
-public class Ex4 {
+import java.util.Arrays;
+public class Ex24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("width = ");
-        float width = sc.nextFloat();
-        System.out.print("height = ");
-        float height = sc.nextFloat();
-        System.out.printf("Diện tích : %.2f\nChu vi: %.2f ",width*height,2*(width+height));
+        int c1 = sc.nextInt();
+        int c2 = sc.nextInt();
+        int c3 = sc.nextInt();
+        int[] arr={c1,c2,c3};
+        Arrays.sort(arr);
+        if(arr[0]<=0){
+            System.out.print("Ba cạnh không tạo thành tam giác");
+        }else if(arr[1]+arr[0] <= arr[2]){
+            System.out.print("Ba cạnh không tạo thành tam giác");
+        }else if(arr[0]==arr[2]){
+            System.out.print("Đây là tam giác đều");
+        }else if(arr[0]*arr[0]+arr[1]*arr[1] == arr[2]*arr[2]){
+            if(arr[0]==arr[1]){
+                System.out.print("Đây là tam giác vuông cân");
+            }else System.out.print("Đây là tam giác vuông");
+        }else  if(arr[0] == arr[1] || arr[2] == arr[1]){
+            System.out.print("Đây là tam giác cân");
+        }else System.out.print("Đây là tam giác thường");
     }
 }
