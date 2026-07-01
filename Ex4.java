d3e31240acd6f80cd0587f803a884873470e8ad2
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-01 18:18:15 +0700

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
-import java.util.Arrays;
-public class Ex24 {
+public class Ex44 {
+    public static void Ex(int[] arr, int countodd){
+        int n=arr.length;
+        int[] arr1=new int[n];
+        int odd_idx=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                arr1[odd_idx]=arr[i];
+                odd_idx++;
+            }else{
+                arr1[countodd]=arr[i];
+                countodd++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr1[i]+" ");
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int c1 = sc.nextInt();
-        int c2 = sc.nextInt();
-        int c3 = sc.nextInt();
-        int[] arr={c1,c2,c3};
-        Arrays.sort(arr);
-        if(arr[0]<=0){
-            System.out.print("Ba cạnh không tạo thành tam giác");
-        }else if(arr[1]+arr[0] <= arr[2]){
-            System.out.print("Ba cạnh không tạo thành tam giác");
-        }else if(arr[0]==arr[2]){
-            System.out.print("Đây là tam giác đều");
-        }else if(arr[0]*arr[0]+arr[1]*arr[1] == arr[2]*arr[2]){
-            if(arr[0]==arr[1]){
-                System.out.print("Đây là tam giác vuông cân");
-            }else System.out.print("Đây là tam giác vuông");
-        }else  if(arr[0] == arr[1] || arr[2] == arr[1]){
-            System.out.print("Đây là tam giác cân");
-        }else System.out.print("Đây là tam giác thường");
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        int n = sc.nextInt();
+        int countodd=0;
+        if(n==0){
+            System.out.print("Mảng không có phần tử");
+            System.exit(0);
+        }
+        System.out.print("Nhập các phần tử của mảng:\n");
+        int[] mang = new int[n];
+        for(int i=1;i<=n;i++){
+            System.out.printf("Phần tử thứ %d: ",i);
+            mang[i-1] = sc.nextInt();
+            if(mang[i-1]%2==0) countodd++;
+        }
+        Ex(mang,countodd);
+
     }
 }
