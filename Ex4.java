f8c6fbc9fd7e73f940eccd981bcc1eb43274c619
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-06 15:02:21 +0700

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -1,39 +1,20 @@
+import java.util.Random;
 import java.util.Scanner;
-public class Ex44 {
-    public static void Ex(int[] arr, int countodd){
-        int n=arr.length;
-        int[] arr1=new int[n];
-        int odd_idx=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]%2==0){
-                arr1[odd_idx]=arr[i];
-                odd_idx++;
-            }else{
-                arr1[countodd]=arr[i];
-                countodd++;
-            }
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(arr1[i]+" ");
-        }
-    }
+public class Ex54 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập số lượng phần tử của mảng: ");
-        int n = sc.nextInt();
-        int countodd=0;
-        if(n==0){
-            System.out.print("Mảng không có phần tử");
-            System.exit(0);
-        }
-        System.out.print("Nhập các phần tử của mảng:\n");
-        int[] mang = new int[n];
-        for(int i=1;i<=n;i++){
-            System.out.printf("Phần tử thứ %d: ",i);
-            mang[i-1] = sc.nextInt();
-            if(mang[i-1]%2==0) countodd++;
-        }
-        Ex(mang,countodd);
+        Scanner sc=new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(generateRandomString(x));
+    }
 
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
     }
 }
